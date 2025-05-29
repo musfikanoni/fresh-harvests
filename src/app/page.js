9b58840e24b8e9bd02f48074ba04ca8270d875f1
musfikanoni <musfikanoni@gmail.com>
2025-05-29 23:16:19 +0600

    about us section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import OurProductsSection from "./components/OurProductsSection";
 import Banner from "./components/Banner";
+import About from "./components/AboutUs/About";
 
 export default function Home() {
   return (
     <div>
       <Banner />
       <OurProductsSection></OurProductsSection>
+      <About></About>
     </div>
   );
 }
