4f9908c2907b5aaf458dc180a82f9249e1296431
musfikanoni <musfikanoni@gmail.com>
2025-05-29 23:59:36 +0600

    update special offer

--- src/app/components/specialOffer/SpecialOffer.jsx
+++ src/app/components/specialOffer/SpecialOffer.jsx
@@ -4,7 +4,7 @@ import CountdownTimer from '../login/components/CountdownTimer'
 
 export default function SpecialOffer() {
   return (
-    <div className='special h-[75vh] lg:w-[1140px]'>
+    <div className='special h-[75vh] w-[490px] lg:w-[1140px]'>
       <div className="mx-auto max-w-7xl">
         <span className='bg-[#759b3f22] text-sm lg:text-base  px-4 py-2.5 rounded text-[#749B3F] font-bold'>Special Offer</span>
         <h2 className='lg:text-4xl text-3xl font-bold my-3'>Seasonal Fruit Bundle</h2>
@@ -12,8 +12,10 @@ export default function SpecialOffer() {
         
 
         <CountdownTimer targetDate="2025-06-02T00:00:00" />
-        <span className='bg-[#176D38] my-5 px-5 py-3 rounded-full font-extrabold'>CODE : <span className='text-warning'>FRESH28</span></span>
+        <div className="mt-7">
+            <span className='bg-[#176D38] px-5 py-3 rounded-full font-extrabold'>CODE : <span className='text-warning'>FRESH28</span></span>
       </div>
+        </div>
       
     </div>
   )
